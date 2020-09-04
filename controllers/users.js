bbfa1fd55d31e99379084b2187f0555dcc7e6ccc
Ken Coppola <kcoppola@ranker.com>
2020-09-04 17:12:57 -0400

    Added the contollers

---
+++ controllers/users.js
@@ -0,0 +1,43 @@
+
+import { v4 as uuidv4 } from 'uuid';
+
+let users = [];
+
+export const createUser = (req, res) => {
+    const user = req.body;
+
+    users.push({...user, id: uuidv4() });
+        res.send(`User with the name ${user.firstName} added to the database.`);
+};
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with the ${id} deleted from the database.`);
+};
+
+export const getUsers = (req, res) => {
+    res.send(users);
+};
+
+export const getUserId = (req, res) => {
+    const { id } = req.params;
+
+    const locatedUser = users.find((user) => user.id === id);
+    res.send(locatedUser);
+};
+
+export const patchUser = (req, res) => {
+    const { id } = req.params;
+    const user = users.find((user) => user.id === id);
+
+    const { firstName, lastName, age } = req.body;
+
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+    
+    res.send(`The user with the id ${id} has been updated.`)
+};
--- routes/users.js
+++ routes/users.js
@@ -1,59 +1,16 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { createUser, deleteUser, getUsers, getUserId, patchUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [
-    {
-        firstName: "John",
-        lastName: "Cooper",
-        age: 24,
-    },
+router.delete('/:id', deleteUser);
 
-    {
-        firstName: "Stephanie",
-        lastName: "Miller",
-        age: 30,
-    }
-];
+router.get('/', getUsers);
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
+router.get('/:id', getUserId);
 
-    users = users.filter((user) => user.id !== id);
+router.patch('/:id', patchUser);
 
-    res.send(`User with the ${id} deleted from the database.`);
-});
-
-router.get('/', (req, res) => {
-    res.send(users);
-});
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-
-    const locatedUser = users.find((user) => user.id === id);
-    res.send(locatedUser);
-});
-
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const user = users.find((user) => user.id === id);
-
-    const { firstName, lastName, age } = req.body;
-
-    if (firstName) user.firstName = firstName;
-    if (lastName) user.lastName = lastName;
-    if (age) user.age = age;
-    
-    res.send(`The user with the id ${id} has been updated.`)
-});
-
-router.post('/', (req, res) => {
-    const user = req.body;
-
-    users.push({...user, id: uuidv4() });
-        res.send(`User with the name ${user.firstName} added to the database.`);
-});
+router.post('/', createUser);
 
 export default router;
